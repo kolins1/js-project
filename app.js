a92705cfa068006547f6b5d5694566a13287f75d
ckipkurui77@gmail.com <ckipkurui77@gmail.com>
2021-11-14 23:10:03 +0300

    changed the navbar

--- app.js
+++ app.js
@@ -26,4 +26,10 @@ function cal() {
 }
 
 
-  
+function submitRadioButton() {
+  var gen=document.forms["my form"] ["gender"];
+  if(gen[0].checked==false&&gen[1].checked==false) {
+      alert("please enter your gender")
+
+  }
+}
