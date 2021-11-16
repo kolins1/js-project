a0950c748aa7f79e13939062b450a1648c88cef4
ckipkurui77@gmail.com <ckipkurui77@gmail.com>
2021-11-17 01:59:53 +0300

    updated the html

--- index.js
+++
@@ -1,7 +0,0 @@
-function submitRadioButton() {
-    var gen=document.forms["my form"] ["gender"];
-    if(gen[0].checked==false&&gen[1].checked==false) {
-        alert("please enter your gender")
-
-    }
-}
